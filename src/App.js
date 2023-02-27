11abecf9efb599ac4d783dbec3f2328d0e4c9035
KalleRas <124579646+KalleRas@users.noreply.github.com>
2023-02-27 21:17:54 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import React, { useState } from 'react'
+
+const URL = 'https://api.exchangerate.host/latest';
+
+function App() {
+  
+  const [eur, setEur] = useState(0);
+  const [gbp, setGbp] = useState(0);
+  const [rate, setRate] = useState(0);
+
+async function convert(e) {
+  e.preventDefault();
+  try {
+
+    const address = URL
+    const response = await fetch(address);
+
+    if (response.ok) {
+      const json = await response.json();
+      setRate(json.rates.GBP);
+      setGbp(eur * json.rates.GBP);
+    } else {
+      alert('Error retrieving exchange rate.');
+    }
+  } catch (err) {
+    alert(err);
+  }
+}
+  return (
+  <div id="container">
+    <form onSubmit={convert}>
+      <div>
+        <label>EUR</label>&nbsp;
+        <input type="number" step="0.01"
+        value={eur} onChange={e => setEur(e.target.value)} />
+        <output>{rate}</output>
+      </div>
+      <div>
+        <label>GBP </label>
+        <output>{gbp.toFixed(2)} €</output>
+      </div>
+      <div>
+        <button>Laske</button>
+      </div>
+    </form>
+  </div>
+  );
+}
+
+export default App;
